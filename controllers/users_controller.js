a03cbf3babb10174dae87edcea10675475a751cf
Raghava <106142347+KV-Raghava@users.noreply.github.com>
2023-03-14 12:17:23 +0530

    make sign up page functional

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
+const User = require('../models/user');
 module.exports.profile = function (req,res){
+
     return res.render('users',{title:'users dummy page'});
 }
 
@@ -20,6 +22,22 @@ module.exports.create = function(req,res){
     if(req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
+    User.findOne({email:req.body.email}).then(user=>{
+        if(!user){
+            User.create(req.body).then(promise=>{
+                return res.redirect('/users/sign-in');
+            }).catch(err => {
+                console.log('error in creating user while signing up');
+                return;
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+    }).catch(error=>{
+        console.log('error in finding user during sign up');
+        return;
+    })
 }
 //sign in and create session
 module.exports.createSession = function(req,res){
