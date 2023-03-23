de6ba0558c98fadb7c5c85cc7604fe4cb259ce04
Raghava <106142347+KV-Raghava@users.noreply.github.com>
2023-03-23 22:19:01 +0530

    make post requests AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,10 +5,19 @@ module.exports.create = function(req, res){
     return res.redirect('back');*/
     //if user is not logged in,redirect him to sign-in page
     if(!req.user){return res.redirect('/users/sign-in');}
+
     Post.create({
         content: req.body.content,
         user: req.user._id
     }).then(user=>{
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post: user
+                },
+                message:'postcreated'
+            });
+        }
         req.flash('success','post published!');
         return res.redirect('back');
     }).catch(error=>{
@@ -18,19 +27,26 @@ module.exports.create = function(req, res){
     })
 }
 
-module.exports.destroy =function(req,res){
-    Post.findById(req.params.id).then(post =>{
+module.exports.destroy =async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id)
         //.id means converting object id into string format
         if(post.user == req.user.id){
             //post.remove();
-            Post.findByIdAndDelete(req.params.id).then(promise => {
-            Comment.deleteMany({post :req.params.id}).then(promise=>{
-                req.flash('success','post deleted');
-                return res.redirect('back');
-            })});
+            
+            await Post.findByIdAndDelete(req.params.id);
+            await Comment.deleteMany({post :req.params.id});
+            req.flash('success','post deleted');
+            return res.redirect('back');
+            
+            
         }
         else{
             return res.redirect('back');
         }
-    })
+    }catch(error){
+        console.log(`error:${error}`);
+        return res.redirect('back');
+    }
+    
 }
